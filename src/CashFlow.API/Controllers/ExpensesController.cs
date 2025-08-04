b6e9925f531446fd390cf11bb6803c83686dcd05
PedroSodreUFES <pedro.malini@edu.ufes.br>
2025-08-04 01:57:40 -0300

    feat: first controller

---
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -0,0 +1,20 @@
+using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : Controller
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    {
+        var useCase = new RegisterExpenseUseCase();
+
+        var response = useCase.Execute(request);
+
+        return Created(string.Empty, response);
+    }
+}
